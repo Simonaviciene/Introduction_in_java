a36f95e0f52a3e1ca41cbee4532ef04edd009976
Simonaviciene <36041581+Simonaviciene@users.noreply.github.com>
2018-03-16 12:07:02 +0100

    Add files via upload

---
+++ pamoka1.java
@@ -0,0 +1,18 @@
+
+public class pamoka1 {
+	public static void main (String []args) {
+		
+		String x;
+		x = "Alabama";
+		int y;
+		y=3;
+		int z;
+		z=2;
+		int sum;
+		sum = y + z;
+		
+		System.out.println(sum);
+		
+	}
+
+}
---
+++ pamoka10is9.java
@@ -0,0 +1,11 @@
+
+public class pamoka10is9 {
+	public static void main(String []args) {
+		pamoka9 objektas = new pamoka9();
+		
+		objektas.zinute();
+		objektas.zinute2();
+		
+	}
+
+}
